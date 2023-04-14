3964cae7b7b758cc94710f197db098a148969008
Harshith Bathula <harshithb59@gmail.com>
2023-04-13 20:24:41 -0400

    updated walsh_like_codes.vhd file

--- vhdl/walsh_like_codes.vhd
+++ vhdl/walsh_like_codes.vhd
@@ -1,1 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity half_adder is
+    port(
+        a, b: in std_logic;
+        sum, carry_out: out std_logic
+    );
+end entity half_adder;
+
+architecture behavioral of half_adder is
+begin
+    sum <= a xor b;
+    carry_out <= a and b;
+end architecture behavioral;
