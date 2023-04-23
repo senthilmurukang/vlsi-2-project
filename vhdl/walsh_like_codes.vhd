56c64a9c591b6a47747a722ad2c89c830d53bbdf
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-23 14:36:57 -0400

    changes for generating internal clock (1:32)

--- vhdl/walsh_like_codes.vhd
+++ vhdl/walsh_like_codes.vhd
@@ -1,35 +1,39 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY walsh_like_codes IS
   PORT (
     SIGNAL clk : IN STD_LOGIC;
-    SIGNAL start : IN STD_LOGIC;
     SIGNAL reset : IN STD_LOGIC;
-    SIGNAL internal_clk : OUT STD_LOGIC := '0'
+    SIGNAL low_speed_clk : OUT STD_LOGIC := '0'
   );
 END ENTITY;
 
 ARCHITECTURE behaviour OF walsh_like_codes IS
+  SIGNAL count_reg : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL min_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL mid_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := "10000";
+  SIGNAL max_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '1');
 BEGIN
-  counter : PROCESS (clk, start, reset)
-    VARIABLE count : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
+  clk_process : PROCESS (clk, reset)
     VARIABLE int_clk : STD_LOGIC := '0';
   BEGIN
-    IF (reset = '1') THEN
-      count := (OTHERS => '0');
-    ELSIF (clk'event AND clk = '1') THEN
-      count := STD_LOGIC_VECTOR(unsigned(count) + 1);
-      IF unsigned(count) >= 2 THEN
-        count := (OTHERS => '0');
-        IF int_clk = '1' THEN
-          int_clk := '0';
-        ELSE
-          int_clk := '1';
-        END IF;
+    IF reset = '1' THEN
+      count_reg <= min_count;
+    ELSIF rising_edge(clk) THEN
+      IF count_reg = max_count THEN
+        count_reg <= min_count;
+      ELSE
+        count_reg <= count_reg + 1;
+      END IF;
+      IF count_reg = mid_count THEN
+        int_clk := '0';
+      ELSIF count_reg = min_count THEN
+        int_clk := '1';
       END IF;
     END IF;
-    internal_clk <= int_clk;
-  END PROCESS counter;
+    low_speed_clk <= int_clk;
+  END PROCESS clk_process;
 END behaviour;
