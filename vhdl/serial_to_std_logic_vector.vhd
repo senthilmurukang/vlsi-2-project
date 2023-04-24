ad7f685fd3439ce8ed87614fd704e1c0a9e0a5a2
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-24 15:55:00 -0400

    modified to take variable length data with generic

--- vhdl/serial_to_std_logic_vector.vhd
+++ vhdl/serial_to_std_logic_vector.vhd
@@ -4,10 +4,11 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY serial_to_std_logic_vector IS
+    GENERIC (DATA_WIDTH : NATURAL := 4);
     PORT (
         SIGNAL clk : IN STD_LOGIC;
         SIGNAL input : IN STD_LOGIC;
-        SIGNAL output : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0')
+        SIGNAL output : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0')
     );
 END ENTITY;
 
@@ -15,7 +16,7 @@ ARCHITECTURE behaviour OF serial_to_std_logic_vector IS
 BEGIN
     PROCESS (clk)
         VARIABLE count : INTEGER := 0;
-        VARIABLE int_output : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
+        VARIABLE int_output : STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
     BEGIN
         IF rising_edge(clk) THEN
             int_output(count) := input;
--- vhdl/std_logic_vector_to_serial.vhd
+++ vhdl/std_logic_vector_to_serial.vhd
@@ -4,9 +4,10 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY std_logic_vector_to_serial IS
+    GENERIC (DATA_WIDTH : NATURAL := 4);
     PORT (
         SIGNAL clk : IN STD_LOGIC;
-        SIGNAL input : IN STD_LOGIC_VECTOR(31 DOWNTO 0) := "10111011101110111011101110111011";
+        SIGNAL input : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
         SIGNAL output : OUT STD_LOGIC := '0'
     );
 END ENTITY;
@@ -17,7 +18,7 @@ BEGIN
     PROCESS
     BEGIN
         WAIT UNTIL rising_edge(clk);
-        IF bit_count = 31 THEN
+        IF bit_count = (DATA_WIDTH - 1) THEN
             output <= input(0);
             bit_count <= 0;
         ELSE
