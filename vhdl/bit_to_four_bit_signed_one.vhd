75e17a1300363104798d08524efd397679d8b670
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-25 01:29:55 -0400

    modified the input singal names

--- vhdl/bit_to_four_bit_signed_one.vhd
+++ vhdl/bit_to_four_bit_signed_one.vhd
@@ -5,10 +5,11 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY bit_to_four_bit_signed_one IS
     PORT (
-        medium_speed_clk : IN STD_LOGIC;
-        low_speed_clk : IN STD_LOGIC;
-        inputBit : IN STD_LOGIC;
-        outputBit : OUT STD_LOGIC);
+        SIGNAL high_speed_clk : IN STD_LOGIC;
+        SIGNAL medium_speed_clk : IN STD_LOGIC;
+        SIGNAL input_bit : IN STD_LOGIC;
+        SIGNAL output_bit : OUT STD_LOGIC
+    );
 END bit_to_four_bit_signed_one;
 
 ARCHITECTURE behavioral OF bit_to_four_bit_signed_one IS
@@ -16,11 +17,11 @@ ARCHITECTURE behavioral OF bit_to_four_bit_signed_one IS
     SIGNAL input_bit_value : BIT := '0';
     SHARED VARIABLE counter : NATURAL := 0;
 BEGIN
-    PROCESS (medium_speed_clk)
+    PROCESS (high_speed_clk)
         VARIABLE positive_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0001";
         VARIABLE negative_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1001";
     BEGIN
-        IF rising_edge(medium_speed_clk) THEN
+        IF rising_edge(high_speed_clk) THEN
             IF input_bit_value = '1' THEN
                 outputReg <= positive_one_bits(counter);
             ELSE
@@ -32,10 +33,10 @@ BEGIN
             END IF;
         END IF;
     END PROCESS;
-    PROCESS (low_speed_clk)
+    PROCESS (medium_speed_clk)
     BEGIN
-        IF rising_edge(low_speed_clk) THEN
-            IF inputBit = '0' THEN
+        IF rising_edge(medium_speed_clk) THEN
+            IF input_bit = '0' THEN
                 input_bit_value <= '0';
             ELSE
                 input_bit_value <= '1';
@@ -43,5 +44,5 @@ BEGIN
             counter := 0;
         END IF;
     END PROCESS;
-    outputBit <= outputReg;
+    output_bit <= outputReg;
 END behavioral;
