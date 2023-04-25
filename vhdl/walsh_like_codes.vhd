395de4187425a6c9a8ba152a5fb332589656ac5f
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-25 01:58:14 -0400

    walsh like encoder with single user

--- vhdl/walsh_like_codes.vhd
+++ vhdl/walsh_like_codes.vhd
@@ -7,7 +7,7 @@ ENTITY walsh_like_codes IS
   PORT (
     SIGNAL clk : IN STD_LOGIC;
     SIGNAL reset : IN STD_LOGIC;
-    SIGNAL output : OUT STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0')
+    SIGNAL output : OUT STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0')
   );
 END ENTITY;
 
@@ -17,7 +17,7 @@ ARCHITECTURE behaviour OF walsh_like_codes IS
   SIGNAL user_data_1 : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1011";
   -- SIGNAL user_data_2 : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1001";
   SIGNAL serial_user_data_1 : STD_LOGIC := '0';
-  SIGNAL walsh_like_code_1 : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1010";
+  SIGNAL walsh_like_code_1 : STD_LOGIC_VECTOR(31 DOWNTO 0) := "10101010101010101010101010101010";
   -- SIGNAL walsh_like_code_2 : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1100";
   SIGNAL encoded_output_1 : STD_LOGIC;
   -- SIGNAL encoded_output_2 : STD_LOGIC;
@@ -39,7 +39,7 @@ BEGIN
       output => serial_user_data_1
     );
   std_logic_vector_to_serial_inst_2 : ENTITY work.std_logic_vector_to_serial
-    GENERIC MAP(DATA_WIDTH => 4)
+    GENERIC MAP(DATA_WIDTH => 32)
     PORT MAP(
       clk => int_medium_clk,
       input => walsh_like_code_1,
@@ -60,13 +60,13 @@ BEGIN
   --   );
   bit_to_four_bit_signed_one_inst : ENTITY work.bit_to_four_bit_signed_one
     PORT MAP(
-      high_speed_clk => int_medium_clk,
-      medium_speed_clk => int_low_clk,
+      high_speed_clk => clk,
+      medium_speed_clk => int_medium_clk,
       input_bit => encoded_output_1,
       output_bit => signed_output_1
     );
   serial_to_std_logic_vector_inst : ENTITY work.serial_to_std_logic_vector
-    GENERIC MAP(DATA_WIDTH => 4)
+    GENERIC MAP(DATA_WIDTH => 32)
     PORT MAP(
       clk => clk,
       input => signed_output_1,
