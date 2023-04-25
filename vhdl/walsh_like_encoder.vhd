87d1102a1644ee804f5f6fd528a1c2cd6ebc6149
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-25 01:11:47 -0400

    fixed the issues with the encoder

--- vhdl/walsh_like_encoder.vhd
+++ vhdl/walsh_like_encoder.vhd
@@ -5,9 +5,7 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY walsh_like_encoder IS
     PORT (
-        SIGNAL high_speed_clk : IN STD_LOGIC;
         SIGNAL medium_speed_clk : IN STD_LOGIC;
-        SIGNAL low_speed_clk : IN STD_LOGIC;
         SIGNAL user_data : IN STD_LOGIC;
         SIGNAL chip_sequence : IN STD_LOGIC;
         SIGNAL encoded_output : OUT STD_LOGIC := '0'
@@ -16,72 +14,44 @@ END ENTITY;
 
 ARCHITECTURE behaviour OF walsh_like_encoder IS
     SIGNAL output_reg : STD_LOGIC := '0';
-    SIGNAL input_bit_value : BIT := '0';
-    SIGNAL user_data_bit : BIT := '0';
-    SIGNAL chip_sequence_bit : BIT := '0';
-    SHARED VARIABLE counter : NATURAL := 0;
-    SHARED VARIABLE user_and_chip_product : BIT := '0';
+    SIGNAL user_data_bit : STD_LOGIC := '0';
+    SIGNAL chip_sequence_bit : STD_LOGIC := '0';
 BEGIN
 
-    signed_bit_generator : PROCESS (high_speed_clk)
-        VARIABLE positive_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0001";
-        VARIABLE negative_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1111";
-    BEGIN
-        IF rising_edge(high_speed_clk) THEN
-            IF input_bit_value = '1' THEN
-                output_reg <= positive_one_bits(counter);
-            ELSE
-                output_reg <= negative_one_bits(counter);
-            END IF;
-            counter := counter + 1;
-            IF counter > 3 THEN
-                counter := 0;
-            END IF;
-        END IF;
-    END PROCESS;
-
-    sample_user_data_chip_product : PROCESS (medium_speed_clk)
-    BEGIN
-        IF rising_edge(medium_speed_clk) THEN
-            IF user_and_chip_product = '0' THEN
-                input_bit_value <= '0';
-            ELSE
-                input_bit_value <= '1';
-            END IF;
-            counter := 0;
-        END IF;
-    END PROCESS;
-
-    sample_chip_bit : PROCESS (medium_speed_clk) IS
-    BEGIN
-        IF rising_edge(medium_speed_clk) THEN
-            IF chip_sequence = '0' THEN
-                chip_sequence_bit <= '0';
-            ELSE
-                chip_sequence_bit <= '1';
-            END IF;
-        END IF;
-    END PROCESS;
-
-    user_data_chip_product : PROCESS (medium_speed_clk)
+    encoder : PROCESS (medium_speed_clk)
     BEGIN
         IF rising_edge(medium_speed_clk) THEN
             IF user_data_bit = '1' THEN
-                user_and_chip_product := chip_sequence_bit;
+                IF chip_sequence_bit = '0' THEN
+                    output_reg <= '0';
+                ELSE
+                    output_reg <= '1';
+                END IF;
             ELSE
-                user_and_chip_product := NOT chip_sequence_bit;
+                IF chip_sequence_bit = '0' THEN
+                    output_reg <= '1';
+                ELSE
+                    output_reg <= '0';
+                END IF;
             END IF;
         END IF;
     END PROCESS;
-
-    sample_input_bit : PROCESS (low_speed_clk)
+    sample_chip_bit : PROCESS (chip_sequence) IS
     BEGIN
-        IF rising_edge(low_speed_clk) THEN
-            IF user_data = '0' THEN
-                user_data_bit <= '0';
-            ELSE
-                user_data_bit <= '1';
-            END IF;
+        IF rising_edge(chip_sequence) THEN
+            chip_sequence_bit <= '1';
+        END IF;
+        IF falling_edge(chip_sequence) THEN
+            chip_sequence_bit <= '0';
+        END IF;
+    END PROCESS;
+    sample_input_bit : PROCESS (user_data)
+    BEGIN
+        IF rising_edge(user_data) THEN
+            user_data_bit <= '1';
+        END IF;
+        IF falling_edge(user_data) THEN
+            user_data_bit <= '0';
         END IF;
     END PROCESS;
     encoded_output <= output_reg;
