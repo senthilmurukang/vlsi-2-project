7383a408e700b0e707b943284fa691a3589410d8
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-26 19:14:07 -0400

    added code to create high speed from fast clock

--- vhdl/internal_clock_generator.vhd
+++ vhdl/internal_clock_generator.vhd
@@ -7,22 +7,47 @@ ENTITY internal_clock_generator IS
     PORT (
         SIGNAL clk : IN STD_LOGIC;
         SIGNAL reset : IN STD_LOGIC;
+        SIGNAL high_speed_clk : OUT STD_LOGIC := '0';
         SIGNAL medium_speed_clk : OUT STD_LOGIC := '0';
         SIGNAL low_speed_clk : OUT STD_LOGIC := '0'
     );
 END ENTITY;
 
 ARCHITECTURE behaviour OF internal_clock_generator IS
-    SIGNAL medium_speed_count_reg : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL medium_speed_min_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL medium_speed_mid_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := "10000";
-    SIGNAL medium_speed_max_count : STD_LOGIC_VECTOR (4 DOWNTO 0) := (OTHERS => '1');
+    SIGNAL high_speed_count_reg : STD_LOGIC_VECTOR (1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL high_speed_min_count : STD_LOGIC_VECTOR (1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL high_speed_mid_count : STD_LOGIC_VECTOR (1 DOWNTO 0) := "10";
+    SIGNAL high_speed_max_count : STD_LOGIC_VECTOR (1 DOWNTO 0) := (OTHERS => '1');
 
-    SIGNAL low_speed_count_reg : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL low_speed_min_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL low_speed_mid_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := "1000000";
-    SIGNAL low_speed_max_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '1');
+    SIGNAL medium_speed_count_reg : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL medium_speed_min_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL medium_speed_mid_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := "1000000";
+    SIGNAL medium_speed_max_count : STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '1');
+
+    SIGNAL low_speed_count_reg : STD_LOGIC_VECTOR (8 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL low_speed_min_count : STD_LOGIC_VECTOR (8 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL low_speed_mid_count : STD_LOGIC_VECTOR (8 DOWNTO 0) := "100000000";
+    SIGNAL low_speed_max_count : STD_LOGIC_VECTOR (8 DOWNTO 0) := (OTHERS => '1');
 BEGIN
+    high_speed_clk_process : PROCESS (clk, reset)
+        VARIABLE int_high_speed_clk : STD_LOGIC := '0';
+    BEGIN
+        IF reset = '1' THEN
+            high_speed_count_reg <= high_speed_min_count;
+        ELSIF rising_edge(clk) THEN
+            IF high_speed_count_reg = high_speed_max_count THEN
+                high_speed_count_reg <= high_speed_min_count;
+            ELSE
+                high_speed_count_reg <= high_speed_count_reg + 1;
+            END IF;
+            IF high_speed_count_reg = high_speed_mid_count THEN
+                int_high_speed_clk := '0';
+            ELSIF high_speed_count_reg = high_speed_min_count THEN
+                int_high_speed_clk := '1';
+            END IF;
+        END IF;
+        high_speed_clk <= int_high_speed_clk;
+    END PROCESS high_speed_clk_process;
     medium_speed_clk_process : PROCESS (clk, reset)
         VARIABLE int_medium_speed_clk : STD_LOGIC := '0';
     BEGIN
