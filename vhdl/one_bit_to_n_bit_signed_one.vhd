278b7e61b9136e58580791930bd109620daf22be
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-26 22:50:51 -0400

    renamed file to support more than four bits

--- vhdl/one_bit_to_n_bit_signed_one.vhd
+++ vhdl/one_bit_to_n_bit_signed_one.vhd
@@ -3,15 +3,15 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY bit_to_four_bit_signed_one IS
+ENTITY one_bit_to_n_bit_signed_one IS
     PORT (
         SIGNAL high_speed_clk : IN STD_LOGIC;
         SIGNAL input_bit : IN STD_LOGIC;
         SIGNAL output_bit : OUT STD_LOGIC
     );
-END bit_to_four_bit_signed_one;
+END one_bit_to_n_bit_signed_one;
 
-ARCHITECTURE behavioral OF bit_to_four_bit_signed_one IS
+ARCHITECTURE behavioral OF one_bit_to_n_bit_signed_one IS
     SIGNAL output_reg : STD_LOGIC := '0';
 BEGIN
     PROCESS (high_speed_clk)
