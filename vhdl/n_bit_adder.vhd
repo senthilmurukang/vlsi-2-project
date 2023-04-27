1d666c1aad3ea3809d19e5ab10f5246280ab8c30
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-27 14:04:41 -0400

    fixed n bit adder

--- vhdl/n_bit_adder.vhd
+++ vhdl/n_bit_adder.vhd
@@ -17,18 +17,20 @@ ENTITY n_bit_adder IS
 END ENTITY;
 
 ARCHITECTURE behavioral OF n_bit_adder IS
-    SIGNAL input_vector_a : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0);
-    SIGNAL input_vector_b : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0);
-    SIGNAL output_vector : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0);
-    SIGNAL input_counter : NATURAL RANGE 0 TO INPUT_WIDTH + 1 := 0;
-    SIGNAL output_counter : NATURAL RANGE 0 TO INPUT_WIDTH + 1 := 0;
+    SIGNAL input_vector_a : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL input_vector_b : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL output_vector : STD_LOGIC_VECTOR (INPUT_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
 BEGIN
     sampling_input : PROCESS (high_speed_clk) IS
+        VARIABLE input_counter : NATURAL RANGE 0 TO INPUT_WIDTH + 1 := 0;
     BEGIN
         IF rising_edge(high_speed_clk) THEN
             input_vector_a(input_counter) <= input_a;
             input_vector_b(input_counter) <= input_b;
-            input_counter <= input_counter + 1;
+            input_counter := input_counter + 1;
+            IF input_counter > (INPUT_WIDTH - 1) THEN
+                input_counter := 0;
+            END IF;
         END IF;
     END PROCESS;
 
@@ -40,9 +42,14 @@ BEGIN
     END PROCESS;
 
     output_generation : PROCESS (high_speed_clk) IS
+        VARIABLE output_counter : NATURAL RANGE 0 TO INPUT_WIDTH + 1 := 0;
     BEGIN
         IF rising_edge(high_speed_clk) THEN
             output_sum <= output_vector(output_counter);
+            output_counter := output_counter + 1;
+            IF output_counter > (INPUT_WIDTH - 1) THEN
+                output_counter := 0;
+            END IF;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
