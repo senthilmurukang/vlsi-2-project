d3a0e987e4be2d65022322407c17fc225fb4e90d
Senthil Murugan Gnanasekaran <senthilmurukang@gmail.com>
2023-04-26 21:55:47 -0400

    actual: fixed synthesis issue of bit to signed one

--- vhdl/bit_to_four_bit_signed_one.vhd
+++ vhdl/bit_to_four_bit_signed_one.vhd
@@ -13,15 +13,14 @@ END bit_to_four_bit_signed_one;
 
 ARCHITECTURE behavioral OF bit_to_four_bit_signed_one IS
     SIGNAL output_reg : STD_LOGIC := '0';
-    SIGNAL input_bit_value : BIT := '0';
-    SHARED VARIABLE counter : NATURAL := 0;
 BEGIN
     PROCESS (high_speed_clk)
-        VARIABLE positive_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0001";
-        VARIABLE negative_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1111";
+        CONSTANT positive_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "0001";
+        CONSTANT negative_one_bits : STD_LOGIC_VECTOR(3 DOWNTO 0) := "1111";
+        VARIABLE counter : NATURAL RANGE 0 TO 4;
     BEGIN
         IF rising_edge(high_speed_clk) THEN
-            IF input_bit_value = '1' THEN
+            IF input_bit = '1' THEN
                 output_reg <= positive_one_bits(counter);
             ELSE
                 output_reg <= negative_one_bits(counter);
@@ -30,15 +29,6 @@ BEGIN
             IF counter > 3 THEN
                 counter := 0;
             END IF;
-        END IF;
-    END PROCESS;
-    PROCESS (input_bit)
-    BEGIN
-        IF rising_edge(input_bit) THEN
-            input_bit_value <= '1';
-        END IF;
-        IF falling_edge(input_bit) THEN
-            input_bit_value <= '0';
         END IF;
     END PROCESS;
     output_bit <= output_reg;
